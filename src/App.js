89cf5c3aee51bfb4a14823951d7e7018983bf058
Sergio Rodriguez <sergrodrig@gmail.com>
2021-06-19 23:10:59 -0400

    fix de import incorrecto en el archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import './App.css';
 
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/container/ItemListContainer.jsx';
